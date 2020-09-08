a9b057d4b3f2b4afa6f7aa6c4fcd307e21554ae3
Multimulder25 <70944502+Multimulder25@users.noreply.github.com>
2020-09-08 11:27:44 +0200

    Create EA_GROUP55.py

---
+++ EA_GROUP55.py
@@ -0,0 +1,141 @@
+################################
+# EvoMan FrameWork - V1.0 2016 #
+# Author: Karine Miras         #
+# karine.smiras@gmail.com      #
+################################
+
+# imports framework
+import sys, os
+sys.path.insert(0, 'evoman') 
+from environment import Environment
+from demo_controller import player_controller
+
+# imports other libs
+import time
+import numpy as np
+from math import fabs,sqrt
+import glob, os
+
+experiment_name = 'dm_dummy_demo'
+if not os.path.exists(experiment_name):
+    os.makedirs(experiment_name)
+
+#number of....
+n_hidden_neurons = 10
+
+# initializes environment with ai player using random controller, playing against static enemy
+env = Environment(experiment_name=experiment_name,
+                  enemies=[2],
+                  playermode="ai",
+                  player_controller=player_controller(n_hidden_neurons),
+                  enemymode="static",
+                  level=2,
+                  speed="fastest")
+
+
+# default environment fitness is assumed for experiment
+env.state_to_log() # checks environment state
+
+ini = time.time()  # sets time marker
+
+#run type train or test 
+run_mode = 'train' # train or test
+
+# number of weights for multilayer with 10 hidden neurons
+n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
+
+#Var pop and NN
+dom_u = 1 #upperbound NN value
+dom_l = -1 #lowerbound NN value
+npop = 5 #population size
+gens = 10 #number of generations
+
+######################function definitions########################3
+
+#simulate with a specific NN
+def simulation(env,x):
+    f,p,e,t = env.play(pcont=x)
+    return f
+
+# evaluation
+def evaluate(x):
+    return np.array(list(map(lambda y: simulation(env,y), x)))
+
+#####################loading or creating a population#####################
+# loads file with the best solution for testing
+if run_mode =='test':
+
+    bsol = np.loadtxt(experiment_name+'/best.txt')
+    print( '\n RUNNING SAVED BEST SOLUTION \n')
+    env.update_parameter('speed','normal')
+    evaluate([bsol])
+
+    sys.exit(0)
+
+
+# initializes population loading old solutions or generating new ones
+
+if not os.path.exists(experiment_name+'/evoman_solstate'):
+
+    print( '\nSTARTING A NEW EVOLUTION\n')
+
+    pop = np.random.uniform(dom_l, dom_u, (npop, n_vars))
+    fit_pop = evaluate(pop)
+    best = np.argmax(fit_pop)
+    mean = np.mean(fit_pop)
+    std = np.std(fit_pop)
+    ini_g = 0
+    solutions = [pop, fit_pop]
+    env.update_solutions(solutions)
+
+else:
+
+    print( '\nCONTINUING WITH AN EVOLUTION\n')
+
+    env.load_state()
+    pop = env.solutions[0]
+    fit_pop = env.solutions[1]
+
+    best = np.argmax(fit_pop)
+    mean = np.mean(fit_pop)
+    std = np.std(fit_pop)
+
+    # finds last generation number
+    file_aux  = open(experiment_name+'/gen.txt','r')
+    ini_g = int(file_aux.readline())
+    file_aux.close()
+
+# saves results for first pop
+file_aux  = open(experiment_name+'/results.txt','a')
+file_aux.write('\n\ngen best mean std')
+print( '\n GENERATION '+str(ini_g)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6)))
+file_aux.write('\n'+str(ini_g)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6))   )
+file_aux.close()
+###################Evolution Algorithm########################
+
+for i in range(ini_g+1, gens):
+    
+
+
+
+
+    ###################save results#####################
+    # saves results
+    file_aux  = open(experiment_name+'/results.txt','a')
+    print( '\n GENERATION '+str(i)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6)))
+    file_aux.write('\n'+str(i)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6))   )
+    file_aux.close()
+
+    # saves generation number
+    file_aux  = open(experiment_name+'/gen.txt','w')
+    file_aux.write(str(i))
+    file_aux.close()
+
+    # saves file with the best solution
+    np.savetxt(experiment_name+'/best.txt',pop[best])
+
+    # saves simulation state
+    solutions = [pop, fit_pop]
+    env.update_solutions(solutions)
+    env.save_state()
+
