03f8a411360c34f6b2de7565cca53317a41f9fe9
Multimulder25 <70944502+Multimulder25@users.noreply.github.com>
2020-09-20 15:12:32 +0200

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -43,44 +43,44 @@ def simulation(env,x):
     return f
 
 #get the new alogirhm
-for i in range(1,runs+1):
-    file = 'test{}.{}'.format(enemy, i)
-    file_location = 'enemy_{}_new30'.format(enemy)
-    experiment_name = (file_location + '/' + file)
-    gainTotal = 0
+#for i in range(1,runs+1):
+#    file = 'test{}.{}'.format(enemy, i)
+#    file_location = 'enemy_{}_new30'.format(enemy)
+#    experiment_name = (file_location + '/' + file)
+#    gainTotal = 0
     
     
     #for j in range(1,6):
-    env = sim_environment(experiment_name, enemy, n_hidden_neurons)
-    bsol = np.loadtxt(file_location + '/' + file + '/best.txt')
-    f,p,e,t = env.play(pcont=bsol)
-    gain1 = p - e
+#    env = sim_environment(experiment_name, enemy, n_hidden_neurons)
+#    bsol = np.loadtxt(file_location + '/' + file + '/best.txt')
+#    f,p,e,t = env.play(pcont=bsol)
+#    gain1 = p - e
     #gainTotal = gainTotal + gain1
     #print(gainTotal)
-    gainscores_new.append(gain1)
+#    gainscores_new.append(gain1)
     #gainscores_new.append(gainTotal/5)
 
 #get the old algorithm
-for i in range(1,runs+1):
-    file = 'test{}.{}'.format(enemy, i)
-    file_location = 'enemy_{}_old30'.format(enemy)
-    experiment_name = (file_location + '/' + file)
-    gainTotal = 0
+#for i in range(1,runs+1):
+#    file = 'test{}.{}'.format(enemy, i)
+#    file_location = 'enemy_{}_old30'.format(enemy)
+#    experiment_name = (file_location + '/' + file)
+#    gainTotal = 0
     
     
     #for j in range(1,6):
-    env = sim_environment(experiment_name, enemy, n_hidden_neurons)
-    bsol = np.loadtxt(file_location + '/' + file + '/best.txt')
-    f,p,e,t = env.play(pcont=bsol)
-    gain1 = p - e
+#    env = sim_environment(experiment_name, enemy, n_hidden_neurons)
+#    bsol = np.loadtxt(file_location + '/' + file + '/best.txt')
+#    f,p,e,t = env.play(pcont=bsol)
+#    gain1 = p - e
     #gainTotal = gainTotal + gain1
     #print(gainTotal)
-    gainscores_old.append(gain1)
+#    gainscores_old.append(gain1)
     #gainscores_new.append(gainTotal/5)
 
-plt.boxplot(gainscores_new)
-plt.boxplot(gainscores_old)
-sys.exit(0)
+#plt.boxplot(gainscores_new)
+#plt.boxplot(gainscores_old)
+#sys.exit(0)
 
 for i in range(1,runs+1):
     file = 'test{}.{}'.format(enemy, i)
@@ -136,24 +136,24 @@ plt.show()
 #plt.fill_between(range(len(result_best["average"])), result_best["average"]-result_std["average"], result_best["average"]+result_std["average"], color='gray', alpha=0.4)
 
 
-A = [[88,76,88,78,78,78,90,78,82,78],  [84,82,82,86,86,76,82,82,84,84]]
-B = [[34,32,48,30,38,12,34,14,26,82],  [-20,40,22,32,12,32,2,20,42,48]]
-C = [[62,-40,-30,47,40,39,58,12,67,-10], [-40,39,38,-30,9,9,-40,6,59,-16]]
+#A = [[88,76,88,78,78,78,90,78,82,78],  [84,82,82,86,86,76,82,82,84,84]]
+#B = [[34,32,48,30,38,12,34,14,26,82],  [-20,40,22,32,12,32,2,20,42,48]]
+#C = [[62,-40,-30,47,40,39,58,12,67,-10], [-40,39,38,-30,9,9,-40,6,59,-16]]
 
-plt.boxplot(A)
+#plt.boxplot(A)
 
 
-df = pd.DataFrame({'Enemy':['2','2','2','2','2','2','2','2','2','2', '3','3','3','3','3','3','3','3','3','3','4','4','4','4','4','4','4','4','4','4'],\
-                  'alg1':[88,76,88,78,78,78,90,78,82,78,34,32,48,30,38,12,34,14,26,82,62,-40,-30,47,40,39,58,12,67,-10],\
-                  'alg2':[84,82,82,86,86,76,82,82,84,84,-20,40,22,32,12,32,2,20,42,48,-40,39,38,-30,9,9,-40,6,59,-16]})
+#df = pd.DataFrame({'Enemy':['2','2','2','2','2','2','2','2','2','2', '3','3','3','3','3','3','3','3','3','3','4','4','4','4','4','4','4','4','4','4'],\
+ #                 'alg1':[88,76,88,78,78,78,90,78,82,78,34,32,48,30,38,12,34,14,26,82,62,-40,-30,47,40,39,58,12,67,-10],\
+#                  'alg2':[84,82,82,86,86,76,82,82,84,84,-20,40,22,32,12,32,2,20,42,48,-40,39,38,-30,9,9,-40,6,59,-16]})
 
-df = df[['Enemy','alg1','alg2']]
+#df = df[['Enemy','alg1','alg2']]
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-dd=pd.melt(df,id_vars=['Enemy'],value_vars=['alg1','alg2'],var_name='algorithm')
-sns.boxplot(x='Enemy',y='value',data=dd,hue='algorithm')
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#dd=pd.melt(df,id_vars=['Enemy'],value_vars=['alg1','alg2'],var_name='algorithm')
+#sns.boxplot(x='Enemy',y='Gain',data=dd,hue='Algorithm')
 
 
 
