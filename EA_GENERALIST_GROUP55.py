bee1e690d26d1c72042afeb7f703b9888db62e20
hensjiepensjie <hanspijpers58@hotmail.com>
2020-10-05 17:18:46 +0200

    Complete algorithm from paper added. Test against all enemies still needs to be implemented. Idea was to read in the best solutions, but numpy has issues

--- EA_GENERALIST_GROUP55.py
+++ EA_GENERALIST_GROUP55.py
@@ -1,8 +1,3 @@
-
-# Author: Devin Mulder        			                                      
-     				                                  
-
-
 # imports framework
 import sys
 sys.path.insert(0, 'evoman')
@@ -16,48 +11,19 @@ from math import fabs,sqrt
 import glob, os
 
 
-n_hidden_neurons = 10
-enemy= [7,8]
+def sim_environment(experiment_name, enemies, n_hidden_neurons):
+    # initializes simulation in multi evolution mode, for multiple static enemies.
+    env = Environment(experiment_name=experiment_name,
+                      enemies=enemies,
+                      multiplemode="yes",
+                      playermode="ai",
+                      player_controller=player_controller(n_hidden_neurons),
+                      enemymode="static",
+                      level=2,
+                      speed="fastest")
 
+    return env
 
-experiment_name = 'multi_demo_2'
-if not os.path.exists(experiment_name):
-    os.makedirs(experiment_name)
-
-# initializes simulation in multi evolution mode, for multiple static enemies.
-env = Environment(experiment_name=experiment_name,
-                  enemies=[2,3],
-                  multiplemode="yes",
-                  playermode="ai",
-                  player_controller=player_controller(n_hidden_neurons),
-                  enemymode="static",
-                  level=2,
-                  speed="fastest")
-
-# default environment fitness is assumed for experiment
-
-env.state_to_log() # checks environment state
-
-
-####   Optimization for controller solution (best genotype-weights for phenotype-network): Ganetic Algorihm    ###
-
-ini = time.time()  # sets time marker
-
-
-# genetic algorithm params
-
-run_mode = 'train' # train or test
-
-# number of weights for multilayer with 10 hidden neurons.
-n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
-
-dom_u = 1
-dom_l = -1
-npop = 5
-gens = 3
-mutation_prob = 0.2  # variable mutation prob
-
-np.random.seed(69)
 
 # runs simulation
 def simulation(env,x):
@@ -65,10 +31,10 @@ def simulation(env,x):
     return f
 
 # evaluation
-def evaluate(x):
+def evaluate(x, env):
     return np.array(list(map(lambda y: simulation(env,y), x)))
 
-def evolution(pop, fit_pop, npop):
+def evolution(pop, fit_pop, npop, number_of_weights, env):
     
     partkilled = int(3*npop/4)  # a quarter of the population
     bestparents = int(npop/4) # a half of the population
@@ -82,7 +48,7 @@ def evolution(pop, fit_pop, npop):
         parent2 = tournament(best_parents, fit_pop)
 
         # crossover
-        for j in range(0,n_vars):
+        for j in range(0, number_of_weights):
             
             prob1 = np.random.uniform(0,1)
 
@@ -91,7 +57,7 @@ def evolution(pop, fit_pop, npop):
             else:
                 pop[x][j] = pop[parent2][j]
 
-            fit_pop[x]=evaluate([pop[x]], env)
+        fit_pop[x] = evaluate([pop[x]], env)
         
     return pop, fit_pop
 
@@ -103,108 +69,252 @@ def tournament(parent_range, fit_pop):
     else:
         return candidate_2
 
-#croos over
-def cross_over():
-    return
 
-def mutation():
-    return
+def new_evolution(pop, fit_pop, npop, gen_pop, gen, total_gens, env):
+    offspring = []
+    n_mutants = int(npop / 2) # Half of population
+    # Crossover
+    # Integer list to make random pairs of parents from full population
+    int_list = np.arange(npop)
+    mating_parents = np.random.choice(int_list, size=(int(npop / 2), 2), replace=False)
+    for pair in mating_parents:
+        child = cross_over(pair, pop)
+        offspring.append(child)
 
-# loads file with the best solution for testing
-if run_mode =='test':
+    # Mutation over parents and children sampled
+    ints_to_mutate = np.random.choice(npop+len(offspring), n_mutants, replace=False)
 
-    bsol = np.loadtxt(experiment_name+'/best.txt')
-    print( '\n RUNNING SAVED BEST SOLUTION \n')
-    env.update_parameter('speed','normal')
-    evaluate([bsol])
+    for integer in ints_to_mutate:
+        # If integer smaller than npop, than mutate parent. Otherwise mutate child.
+        if integer < npop:
+            mutant = pop[integer]
+            sigma = (total_gens - gen) / total_gens
+
+        else:
+            mutant = offspring[integer-npop]
+            sigma = 0.5
+
+        child = mutation(mutant, sigma)
+        offspring.append(child)
+
+    fit_offspring = evaluate(offspring, env)
+
+    # Best npop of parents and children stay alive
+    # Copy lists and concatenate
+    current_gen = np.array([gen]*npop)
+    temp_pop = np.concatenate((pop, offspring))
+    temp_fit = np.concatenate((fit_pop, fit_offspring))
+    temp_gen_pop = np.concatenate((gen_pop, current_gen))
+
+    surviving_order = np.argsort(temp_fit)
+    # Best npop become new population
+    pop = temp_pop[surviving_order][-npop:]
+    fit_pop = temp_fit[surviving_order][-npop:]
+    gen_pop = temp_gen_pop[surviving_order][-npop:]
+
+    return pop, fit_pop, gen_pop
+
+
+# convex combination of two parents
+def cross_over(pair, pop):
+    random_1 = np.random.uniform()
+    random_2 = np.random.uniform()
+
+    child = pop[pair[0]]*random_1 + pop[pair[1]]*random_2
+
+    return child
+
+
+# Add value from gaussian distribution with 0 mean and sigma standard deviation to weight
+def mutation(mutant, sigma):
+
+    # Loop over each weight
+    for i in range(len(mutant)):
+        mutant[i] = mutant[i] + np.random.normal(loc=0.0, scale=sigma)
+
+    return mutant
+
+
+def run_simulation(args):
+    np.random.seed(69)
+    n_hidden_neurons = args.n_neurons
+
+    ini = time.time()  # sets time marker
+
+    enemies = args.enemies
+
+    # run type train or test
+    run_mode = args.run_mode  # train or test or trainten
+
+    # run type train or test
+    if run_mode == 'test':
+        experiment_name = args.experiment_name  # train or test or trainten
+
+    dom_u = 1
+    dom_l = -1
+    npop = args.npop  # population size
+    gens = args.gens  # number of generations
+
+    mutation_prob = 0.2  # variable mutation prob
+
+    # loads file with the best solution for testing
+    if run_mode == 'test':
+        env = sim_environment(experiment_name, enemies, n_hidden_neurons)
+        bsol = np.loadtxt(experiment_name + '/best.txt')
+        print('\n RUNNING SAVED BEST SOLUTION \n')
+        env.update_parameter('speed', 'normal')
+        evaluate([bsol], env)
+
+        sys.exit(0)
+
+    if run_mode == 'trainten':
+        total_runs = 10
+    else:
+        total_runs = 1
+
+    for runs in range(total_runs):
+        if run_mode == 'trainten':
+            experiment_name = 'multi_demo_{}.{}'.format(enemies, runs + 1)
+            if not os.path.exists(experiment_name):
+                os.makedirs(experiment_name)
+        elif run_mode == 'train':
+            experiment_name = args.experiment_name
+            if not os.path.exists(experiment_name):
+                os.makedirs(experiment_name)
+
+        # initializes environment with ai player using random controller, playing against static enemy
+        env = sim_environment(experiment_name, enemies, n_hidden_neurons)
+
+        # default environment fitness is assumed for experiment
+
+        env.state_to_log()  # checks environment state
+
+        ####   Optimization for controller solution (best genotype-weights for phenotype-network): Ganetic Algorihm    ###
+
+        # number of weights for multilayer with 10 hidden neurons.
+        number_of_weights = (env.get_num_sensors() + 1) * n_hidden_neurons + (n_hidden_neurons + 1) * 5
+
+        # evolution
+
+        # initializes population loading old solutions or generating new ones
+        if not os.path.exists(experiment_name + '/evoman_solstate'):
+
+            print('\nNEW EVOLUTION\n')
+
+            pop = np.random.uniform(dom_l, dom_u, (npop, number_of_weights))
+            gen_pop = np.zeros(npop, dtype='int') # List of numbers at which generation someone was born
+            fit_pop = evaluate(pop, env)
+            best = np.argmax(fit_pop)
+            mean = np.mean(fit_pop)
+            std = np.std(fit_pop)
+            ini_g = 0
+            solutions = [pop, fit_pop, gen_pop]
+            env.update_solutions(solutions)
+
+        else:
+
+            print('\nCONTINUING EVOLUTION\n')
+
+            env.load_state()
+            pop = env.solutions[0]
+            fit_pop = env.solutions[1]
+            gen_pop = env.solutions[2]
+
+            best = np.argmax(fit_pop)
+            mean = np.mean(fit_pop)
+            std = np.std(fit_pop)
+
+            # finds last generation number
+            file_aux = open(experiment_name + '/gen.txt', 'r')
+            ini_g = int(file_aux.readline())
+            file_aux.close()
+
+        # saves results for first pop
+        file_aux = open(experiment_name + '/results.txt', 'a')
+        file_aux.write('\n\ngen best mean std best_sol')
+        print(
+            '\n GENERATION ' + str(ini_g) + ' ' + str(round(fit_pop[best], 6)) + ' ' + str(round(mean, 6)) + ' ' + str(
+                round(std, 6)))
+        file_aux.write('\n' + str(ini_g) + ' ' + str(round(fit_pop[best], 6)) + ' ' + str(round(mean, 6)) + ' ' + str(
+            round(std, 6)))
+        file_aux.close()
+
+        # saves results for first pop
+        file_aux = open(experiment_name + '/bestsols.txt', 'a')
+        file_aux.write('gen best_solution')
+        file_aux.write('\n' + str(ini_g) + ' ' + str(pop[best]))
+        file_aux.close()
+
+        last_sol = fit_pop[best]
+        notimproved = 0
+        results = []
+
+        for i in range(ini_g+1, gens):
+
+            pop, fit_pop, gen_pop = new_evolution(pop, fit_pop, npop, gen_pop, i, gens, env)
+            best = np.argmax(fit_pop)  # best solution in generation
+
+            fit_pop[best] = float(evaluate(np.array([pop[best]]), env)[0])  # repeats best eval, for stability issues
+            best_sol = fit_pop[best]
+
+            best = np.argmax(fit_pop)
+            std  = np.std(fit_pop)
+            mean = np.mean(fit_pop)
+
+            results.append(fit_pop[best])
+
+            # saves results
+            file_aux  = open(experiment_name+'/results.txt','a')
+            print( '\n GENERATION '+str(i)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6)))
+            file_aux.write('\n'+str(i)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6)))
+            file_aux.close()
+
+            file_aux = open(experiment_name + '/bestsols.txt', 'a')
+            file_aux.write('\n' + str(i) + ' ' + str(pop[best]))
+            file_aux.close()
+
+            # saves generation number
+            file_aux  = open(experiment_name+'/gen.txt','w')
+            file_aux.write(str(i))
+            file_aux.close()
+
+            # saves file with the best solution
+            np.savetxt(experiment_name+'/best.txt', pop[best])
+
+            # saves simulation state
+            solutions = [pop, fit_pop, gen_pop]
+            env.update_solutions(solutions)
+            env.save_state()
+
+        fim = time.time() # prints total execution time for experiment
+        print( '\nExecution time: '+str(round((fim-ini)/60))+' minutes \n')
+
+        file = open(experiment_name+'/neuroended', 'w')  # saves control (simulation has ended) file for bash loop file
+        file.close()
+
+    full_training_test(args)
+
+def full_training_test(args):
+    env = sim_environment(args.experiment_name, [1,2,3,4,5,6,7,8], args.n_hidden_neurons)
+    bsol = np.loadtxt(args.experiment_name + '/bestsols.txt')
+    print('\n RUNNING SAVED BEST SOLUTION \n')
+    env.update_parameter('speed', 'normal')
+    evaluate([bsol], env)
 
     sys.exit(0)
 
-# initializes population loading old solutions or generating new ones
-if not os.path.exists(experiment_name+'/evoman_solstate'):
 
-    print( '\nNEW EVOLUTION\n')
+if __name__ == '__main__':
+    import argparse
+    import sys
 
-    pop = np.random.uniform(dom_l, dom_u, (npop, n_vars))
-    fit_pop = evaluate(pop)
-    best = np.argmax(fit_pop)
-    mean = np.mean(fit_pop)
-    std = np.std(fit_pop)
-    ini_g = 0
-    solutions = [pop, fit_pop]
-    env.update_solutions(solutions)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n_neurons', '--n_neurons', default=10, type=int)
+    parser.add_argument('-enemies', '--enemies', nargs='+', default=[2, 3])
+    parser.add_argument('-npop', '--npop', default=40, type=int),
+    parser.add_argument('-gens', '--gens', type=int, default=20)
+    parser.add_argument('-run_mode', '--run_mode', default='train')
+    parser.add_argument('-experiment_name', '--experiment_name', default='multi_demo')
+    args = parser.parse_args(sys.argv[1:])
 
-else:
-
-    print( '\nCONTINUING EVOLUTION\n')
-
-    env.load_state()
-    pop = env.solutions[0]
-    fit_pop = env.solutions[1]
-
-    best = np.argmax(fit_pop)
-    mean = np.mean(fit_pop)
-    std = np.std(fit_pop)
-
-    # finds last generation number
-    file_aux  = open(experiment_name+'/gen.txt','r')
-    ini_g = int(file_aux.readline())
-    file_aux.close()
-
-
-
-
-# saves results for first pop
-file_aux  = open(experiment_name+'/results.txt','a')
-file_aux.write('\n\ngen best mean std')
-print( '\n GENERATION '+str(ini_g)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6)))
-file_aux.write('\n'+str(ini_g)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6))   )
-file_aux.close()
-
-
-# evolution
-
-last_sol = fit_pop[best]
-notimproved = 0
-
-for i in range(ini_g+1, gens):
-
-    pop, fit_pop = evolution(pop, fit_pop, npop)
-
-    best = np.argmax(fit_pop)  # best solution in generation
-    fit_pop[best] = float(evaluate(np.array([pop[best]]))[0])  # repeats best eval, for stability issues
-    best_sol = fit_pop[best]
-
-    best = np.argmax(fit_pop)
-    std  =  np.std(fit_pop)
-    mean = np.mean(fit_pop)
-
-
-    # saves results
-    file_aux  = open(experiment_name+'/results.txt','a')
-    print( '\n GENERATION '+str(i)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6)))
-    file_aux.write('\n'+str(i)+' '+str(round(fit_pop[best],6))+' '+str(round(mean,6))+' '+str(round(std,6))   )
-    file_aux.close()
-
-    # saves generation number
-    file_aux  = open(experiment_name+'/gen.txt','w')
-    file_aux.write(str(i))
-    file_aux.close()
-
-    # saves file with the best solution
-    np.savetxt(experiment_name+'/best.txt',pop[best])
-
-    # saves simulation state
-    solutions = [pop, fit_pop]
-    env.update_solutions(solutions)
-    env.save_state()
-
-fim = time.time() # prints total execution time for experiment
-print( '\nExecution time: '+str(round((fim-ini)/60))+' minutes \n')
-
-
-file = open(experiment_name+'/neuroended', 'w')  # saves control (simulation has ended) file for bash loop file
-file.close()
-
-
-env.state_to_log() # checks environment state
+    run_simulation(args)
