308b779bbf7756021f1e19dd2f13540165910aa2
hensjiepensjie <hanspijpers58@hotmail.com>
2020-10-08 09:43:37 +0200

    2358 results tournament

--- EA_GENERALIST_GROUP55.py
+++ EA_GENERALIST_GROUP55.py
@@ -354,7 +354,7 @@ def full_training_test(experiment_name, number_of_weights, n_hidden_neurons, ini
     file_aux.close()
 
     # If no best_final_solution is found after continuing, quit
-    if not best_final_solution.any():
+    if len(best_final_solution) == 0:
         print('no better solution found than previous')
         sys.exit(0)
 
